23c7d8309f9817aa5b37730813aaa07b7d155928
Sukirno <mblonyox@gmail.com>
2022-02-27 12:22:00 +0700

    initial Commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import HandContainer from "./components/HandContainer.vue"
+let hands = []
+</script>
+
+<template>
+  <div class="board">
+    <HandContainer />
+  </div>
+</template>
+
+<style scoped>
+.board {
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  background: radial-gradient(#912a04, #3a1901);
+}
+</style>
---
+++ src/components/HandContainer.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import Card from "./Card.vue";
+const card_num = ref(5);
+</script>
+
+<template>
+  <button class="draw-button" @click="card_num += 1">Draw a card</button>
+  <div class="hand-container">
+    <TransitionGroup name="hand">
+      <div class="card-container" v-for="item in card_num" :key="item">
+        <Card />
+      </div>
+    </TransitionGroup>
+  </div>
+</template>
+
+<style scoped>
+.hand-container {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 200px;
+  border: 1px dashed white;
+  display: flex;
+  justify-content: center;
+}
+
+.card-container {
+  width: 175px;
+  min-width: 20px;
+  max-width: 200px;
+}
+
+.hand-enter-active,
+.hand-leave-active {
+  transition: all 0.3s ease;
+}
+
+.hand-enter-from,
+.hand-leave-to {
+  width: 20px;
+  opacity: 0;
+  transform: translateY(-100px);
+}
+</style>
